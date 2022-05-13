699ba6718057c4f7e679df48e45af2b319fc42ae
Deepak <deezpatra2015@gmail.com>
2022-05-13 17:55:42 +0530

    setmatrix

--- setmatrixzero1.cpp
+++ setmatrixzero1.cpp
@@ -2,26 +2,6 @@
 #include <iostream>
 using namespace std;
 const int m=4, n=3;
-
-
-// void setmatrixzero(int arr[m][n],int a, int b, int n, int m)
-// {
-//     for(int i=0;i<n;i++)
-//     {
-//         arr[i][b]=0;
-//     }
-//     for (int j = 0; j < m; j++)
-//     {
-//         arr[a][j]=0;
-//     }
-
-// void setmatrixzero(int arr[m][n],int a, int b, int n, int m)
-// {
-//     for(int i=0;i<m;i++)
-//     {
-//         for (int j = 0; j < n; j++)
-//         {
-    
 void setmatrix(int arr[m][n],int a, int b, int n, int m)
 {
     for(int i=0;i<m;i++)
@@ -47,7 +27,7 @@ void checkzero(int arr[m][n], int n, int m){
         {
             if (arr[i][j]==0)
             {
-                // setmatrixzero(arr,i,j,n,m);
+               
                 setmatrix(arr,i,j,m,n);
                 
             }
@@ -60,7 +40,7 @@ int main()
 {
 
     int arr[m][n]={{1,2,3},{4,0,5},{5,5,1},{2,2,9}};
-    // 
+    
     
     checkzero(arr,m,n);
 
